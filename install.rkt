925b9d663a86af898fbd222c6f5c07497d9324c4
Maciej Barć <xgqt@riseup.net>
2021-07-09 20:06:30 +0200

    install.rkt: add
    Signed-off-by: Maciej Barć <xgqt@riseup.net>

---
+++ install.rkt
@@ -0,0 +1,37 @@
+#!/usr/bin/env racket
+
+
+;; This file is part of racket-mike.
+
+;; racket-mike is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, version 3.
+
+;; racket-mike is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with racket-mike.  If not, see <https://www.gnu.org/licenses/>.
+
+;; Copyright (c) 2021, Maciej Barć <xgqt@riseup.net>
+;; Licensed under the GNU GPL v3 License
+;; SPDX-License-Identifier: GPL-3.0-only
+
+
+#lang racket/base
+
+(require
+ pkg/lib
+ "mike/private/rules.rkt"
+ )
+
+(module+ main
+  (when (member (PACKAGE_NAME) (installed-pkg-names))
+    (announce (PACKAGE_NAME) "found, uninstalling" (PACKAGE_NAME) "...")
+    (remove)
+    )
+  (announce "Installing" (PACKAGE_NAME) "...")
+  (install)
+  )
