f50a85145d4931b483ccd6febfcab34578c72086
Maciej Barć <xgqt@gentoo.org>
2022-06-08 22:42:22 +0200

    mike/private/rules.rkt: format macros
    Signed-off-by: Maciej Barć <xgqt@gentoo.org>

--- mike/private/rules.rkt
+++ mike/private/rules.rkt
@@ -39,8 +39,7 @@
  (all-from-out "variables.rkt"))
 
 
-(define/contract
-  (recursively-delete dirname start-path)
+(define/contract (recursively-delete dirname start-path)
   (-> string? path-string? void)
   (for ([dir (reverse (find-files directory-exists? start-path))])
     (when (equal? (basename dir) dirname)
@@ -67,7 +66,8 @@
   (displayln (string-join (sort (hash-keys rules) string<?) "\n")))
 
 
-(define announcer (make-parameter "M-> "))
+(define announcer
+  (make-parameter "M-> "))
 
 (define/contract (announce . strs)
   (->* () #:rest (listof string?) void)
@@ -89,58 +89,76 @@
 
 
 ;; Main
+
 (define-rule all (install) (setup) (test))
-(define-rule compile (announce "compiling" (PWD)) (compile-directory (PWD)))
+
+(define-rule compile
+  (announce "compiling" (PWD)) (compile-directory (PWD)))
+
 (define-rule run
   (execute (RACKET) (RACKET_RUN_FLAGS) (ENTRYPOINT) (RUN_FLAGS)))
-(define-rule
-  install
+
+(define-rule install
   (execute (RACO) "pkg install" (INSTALL_FLAGS) "--name" (PACKAGE_NAME)))
 
+
 ;; Documentation
+
 (define-rule docs-dir
   (when (not (or (file-exists? (PACKAGE_DOC_DIR))
                 (directory-exists? (PACKAGE_DOC_DIR))))
     (announce "creating" (PACKAGE_DOC_DIR))
     (make-directory* (PACKAGE_DOC_DIR))))
+
 (define-rule docs-html
   (docs-dir)
   (execute (SCRBL) "--htmls" (SCRBL_FLAGS) (PACKAGE_SCRBL))
   (make-index-redirect (PACKAGE_DOC_DIR) (PACKAGE_NAME)))
+
 (define-rule docs-latex
   (docs-dir)
   (execute (SCRBL) "--latex" (SCRBL_FLAGS) (PACKAGE_SCRBL)))
+
 (define-rule docs-markdown
   (docs-dir)
   (execute (SCRBL) "--markdown" (SCRBL_FLAGS) (PACKAGE_SCRBL)))
+
 ;; To generate PDFs on Gentoo you will need dev-texlive/texlive-fontsextra
+
 (define-rule docs-pdf
   (docs-dir)
   (execute (SCRBL) "--pdf" (SCRBL_FLAGS) (PACKAGE_SCRBL)))
+
 (define-rule docs-text
   (docs-dir)
   (execute (SCRBL) "--text" (SCRBL_FLAGS) (PACKAGE_SCRBL)))
+
 (define-rule docs
   (spin (lambda () (docs-html))
         (lambda () (docs-latex))
         (lambda () (docs-markdown))
         (lambda () (docs-pdf))
         (lambda () (docs-text))))
+
 (define-rule redocs (clean-doc) (docs))
 
+
 ;; Distribution
+
 (define-rule exe
   (compile)
   (make-directory* "./bin")
   (execute (RACO) "exe" (EXE_FLAGS) (ENTRYPOINT)))
+
 (define-rule pkg (clean) (execute (RACO) "pkg create --source" (PWD)))
-(define-rule
-  git-archive
+
+(define-rule git-archive
   (execute (GIT) "archive --output" (PACKAGE_TAR) "--format=tar HEAD"))
 
+
 ;; Removal
-(define-rule
-  distclean
+
+(define-rule distclean
   (announce "removing" (PACKAGE_BIN_DIR) "and" (PACKAGE_TAR) "/" (PACKAGE_ZIP))
   (spin (lambda ()
           (when (directory-exists? (PACKAGE_BIN_DIR))
@@ -151,35 +169,54 @@
         (lambda ()
           (when (file-exists? (PACKAGE_ZIP))
             (delete-file (PACKAGE_ZIP))))))
+
 (define-rule clean-compiled
   (announce "removing compiled artifacts")
   (recursively-delete "compiled" (PWD)))
+
 (define-rule clean-doc
   (announce "removing built documentation")
   (recursively-delete "doc" (PWD)))
-(define-rule remove (execute (RACO) "pkg remove" (DO_DOCS) (PACKAGE_NAME)))
+
+(define-rule remove
+  (execute (RACO) "pkg remove" (DO_DOCS) (PACKAGE_NAME)))
+
 (define-rule clean (clean-compiled) (clean-doc) (distclean))
+
 (define-rule purge (remove) (clean))
+
 (define-rule reinstall (remove) (install))
+
 (define-rule resetup (reinstall) (setup))
 
+
 ;; Tests
+
 (define-rule setup
   (execute (RACO)
            "setup --tidy --avoid-main"
            (DEPS_FLAGS)
            "--pkgs"
            (PACKAGE_NAME)))
+
 (define-rule check-deps
   (execute (RACO) "setup" (DO_DOCS) (DEPS_FLAGS) (PACKAGE_NAME)))
-(define-rule test-local (execute (RACO) "test" (TEST_FLAGS) (PWD)))
+
+(define-rule test-local
+  (execute (RACO) "test" (TEST_FLAGS) (PWD)))
+
 (define-rule test
   (execute (RACO) "test" (TEST_FLAGS) "--package" (PACKAGE_NAME)))
 
+
 ;; Everything
+
 (define-rule show-rules (display-rules))
+
 (define-rule show-variables (display-variables))
+
 (define-rule show (show-rules) (show-variables))
+
 (define-rule everything-test
   (clean)
   (compile)
@@ -188,4 +225,5 @@
   (check-deps)
   (test)
   (purge))
+
 (define-rule everything-dist (pkg) (exe))
